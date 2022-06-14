bdeb4b79370dc26ef9de4da01bbc2874de7c3659
Dustin Firebaugh <dafirebaugh@gmail.com>
2022-06-14 19:48:45 -0400

    updating web documentation `run_local.sh` script

--- web/run_local.sh
+++ web/run_local.sh
@@ -3,7 +3,7 @@
 DIR="web/node_modules"
 if [ ! -d "$DIR" ]; then
   echo "the dependencies need to be installed ${DIR}..."
-  cd web && npm install
+  npm install
 fi
 
-cd web && npm start
+npm start
