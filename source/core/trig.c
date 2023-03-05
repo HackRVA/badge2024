a55f20118f7cf1ee503b169bf726ed2864ff5c22
Stephen M. Cameron <stephenmcameron@gmail.com>
2023-03-05 14:19:29 -0500

    Add arctan2() function
    Signed-off-by: Stephen M. Cameron <stephenmcameron@gmail.com>

--- source/core/trig.c
+++ source/core/trig.c
@@ -22,3 +22,44 @@ short cosine(int a)
 {
 	return sine_array[(a + 32) & 127];
 }
+
+/* Lookup table for angles 0 - 45 degrees (0 to 16 in our system).  x must be greater than or equal to y */
+static int16_t atan_lookup_table(int16_t x, int16_t y)
+{
+	static unsigned char atan_lut[] = { 0, 1, 2, 3, 4, 5, 7, 8, 9, 10, 11, 12, 13, 13, 14, 15, 15 };
+	if (x == 0)
+		return 0;
+	/* x>=0, y>=0 and x>=y, so y/x will be in the range 0.0 to 1.0, so (16*y)/x will be in range 0 - 16 */
+	return (int16_t) atan_lut[(16 * y) / x];
+}
+
+int16_t arctan2(int16_t y, int16_t x)
+{
+	int16_t angle, quadrant = 0;
+
+	if (x < 0) {
+		x = -x;
+		quadrant = 1;
+	}
+	if (y < 0) {
+		y = -y;
+		quadrant |= 2;
+	}
+	if (x > y) /* angle is between 0 and 45 degrees */
+		angle = atan_lookup_table(x, y);
+	else /* angle is between 45 and 90 degrees */
+		angle = 32 - atan_lookup_table(y, x);
+
+	switch (quadrant) {
+	case 0:
+		return angle;
+	case 1:
+		return 64 - angle;
+	case 2:
+		return -angle;
+	case 3:
+	default:
+		return angle - 64;
+	}
+}
+
--- source/core/trig.h
+++ source/core/trig.h
@@ -1,7 +1,7 @@
 #ifndef TRIG_H__
 #define TRIG_H__
 
-/* Sine and cosine, adapted for non-floating point badge use.
+/* Sine, cosine and arctan2, adapted for non-floating point badge use.
  *
  * The angle should be in the range 0-127 (mapped to 0 to 360 degrees).
  * The returned value is the sin or cosine * 256, as a signed short. 
@@ -17,9 +17,11 @@
  * vx = (v * cosine(a)) / 256;  // or >> 8. Compiler is probably smart enough to do that anyway.
  * vy = (v * -sine(a)) / 256; 
  *
+ * The angles returned by arctan2 are in the range -64 to +64, corresponding to -180 and +180 degrees.
  */
 
 short cosine(int angle);
 short sine(int angle);
+short arctan2(short y, short x);
 
 #endif
