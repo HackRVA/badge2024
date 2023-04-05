75539ec006c8c5e5e256f1bc973b96e5b40b89b3
Peter Maxwell Warasila <madmaxwell@soundcomesout.com>
2023-04-05 18:57:32 -0400

    init: add hard fault handler which dumps to lcd
    Signed-off-by: Peter Maxwell Warasila <madmaxwell@soundcomesout.com>

--- source/hal/init_rp2040.c
+++ source/hal/init_rp2040.c
@@ -8,6 +8,7 @@
 #include "hardware/watchdog.h"
 #include "hardware/sync.h"
 #include "hardware/pwm.h"
+#include "hardware/exception.h"
 #include "pinout_rp2040.h"
 #include "display.h"
 #include "led_pwm.h"
@@ -15,6 +16,68 @@
 #include "ir.h"
 #include "rtc.h"
 #include "audio.h"
+
+#include <framebuffer.h>
+#include <colors.h>
+#include <utils.h>
+#include <stdio.h>
+
+static const char *const stacked_reg[] = {
+    "r0",
+    "r1",
+    "r2",
+    "r3",
+    "r12",
+    "lr",
+    "pc",
+    "psr",
+};
+
+void show_reg(const char *const name, uint32_t reg)
+{
+    char msg[16] = {0};
+    snprintf(msg, sizeof(msg), "%3s:%08x\n", name, reg);
+    FbWriteString(msg);
+}
+
+void dump_fault(uint32_t *sp)
+{
+
+    FbClear();
+    FbMove(0,0);
+    FbColor(RED);
+    FbWriteString("HARD FAULT\n");
+
+    for (size_t i = 0; i < ARRAY_SIZE(stacked_reg); i++) {
+        show_reg(stacked_reg[i], sp[i]);
+    }
+
+    show_reg("sp", (uint32_t) sp);
+
+    FbSwapBuffers();
+
+    /* Mute audio */
+    audio_init();
+
+    while(1);
+}
+
+__attribute__((naked)) void hard_fault_handler(void)
+{
+    __asm volatile (
+        "   mov r0, lr          \n"
+        "   ldr r1, mask_lr     \n"
+        "   tst r0, r1          \n"
+        "   bne stack_psp       \n"
+        "   mrs r0, msp         \n"
+        "   b b_dump_fault      \n"
+        "stack_psp:             \n"
+        "   mrs r0, psp         \n"
+        "b_dump_fault:          \n"
+        "   b dump_fault        \n"
+        "mask_lr: .word 0x00000004 \n"
+    );
+}
 
 _Noreturn void core1_procedure(void) {
     // For now, nothing for core 1 to do except allow lockout and sleep forever.
@@ -42,6 +105,8 @@ void hal_init(void) {
     display_reset();
     rtc_init_badge(0);
     audio_init();
+
+    exception_set_exclusive_handler(HARDFAULT_EXCEPTION, hard_fault_handler);
 
     // allow suspend from other core, if we have it run something that needs to do that
     multicore_lockout_victim_init();
