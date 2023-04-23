919a252b1779ede1f1a00e5ff2416234d46f3345
Stephen M. Cameron <stephenmcameron@gmail.com>
2023-04-23 17:59:56 -0400

    username: make it aware of more buttons
    Signed-off-by: Stephen M. Cameron <stephenmcameron@gmail.com>

--- source/apps/username.c
+++ source/apps/username.c
@@ -209,7 +209,9 @@ static void check_the_buttons(void)
 		if (current_col > 5)
 			current_col = 0;
 		something_changed = 1;
-	} else if (BUTTON_PRESSED(BADGE_BUTTON_ENCODER_SW, down_latches)) {
+	} else if (BUTTON_PRESSED(BADGE_BUTTON_ENCODER_SW, down_latches) ||
+		BUTTON_PRESSED(BADGE_BUTTON_A, down_latches) ||
+		BUTTON_PRESSED(BADGE_BUTTON_B, down_latches)) {
 		action = row_col_to_letter(current_row, current_col);
 		if (action == -1) { /* exit */
 			app_state = EXIT_APP;
