a48ce0c8a7eefe2d78775bed81b4f9621fb600f4
Stephen M. Cameron <stephenmcameron@gmail.com>
2023-05-14 14:06:28 -0400

    screensaver: fix backward screen inversion
    Signed-off-by: Stephen M. Cameron <stephenmcameron@gmail.com>

--- source/core/badge.c
+++ source/core/badge.c
@@ -190,10 +190,10 @@ uint64_t ProcessIO(void)
         FbSwapBuffers();
         if(!screensaver_inverted) {
             if(display_get_display_mode() == DISPLAY_MODE_NORMAL) {
-                display_set_display_mode_inverted();
+                display_set_display_mode_noninverted();
             }
             else {
-                display_set_display_mode_noninverted();
+                display_set_display_mode_inverted();
             }
         }
     }
