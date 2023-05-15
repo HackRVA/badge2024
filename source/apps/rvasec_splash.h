933aa4fc0561a80d075af892431ca0d7a14e9f01
Stephen M. Cameron <stephenmcameron@gmail.com>
2023-05-15 08:30:45 -0400

    Move rvasec_splash_cb into a badge app
    It *is* an app, after all, so what's it doing in menu.c?
    
    Signed-off-by: Stephen M. Cameron <stephenmcameron@gmail.com>

---
+++ source/apps/rvasec_splash.h
@@ -0,0 +1,6 @@
+#ifndef RVASEC_SPLASH_H__
+#define RVASEC_SPLASH_H__
+
+void rvasec_splash_cb(struct menu_t *m);
+
+#endif
--- source/core/menu.c
+++ source/core/menu.c
@@ -31,6 +31,7 @@
 #include "asteroids.h"
 #include "etch-a-sketch.h"
 #include "magic-8-ball.h"
+#include "rvasec_splash.h"
 #include "test-screensavers.h"
 
 #define MAIN_MENU_BKG_COLOR GREY2
@@ -74,7 +75,6 @@ static const struct menu_t main_m[] = {
 
 /* hack for badge.c to trigger redraw of menu on transition from dormant -> not dormant */
 unsigned char menu_redraw_main_menu = 0;
-static void rvasec_splash_cb();
 
 /* Frequency in Hz of beeps to make for various menu actions */
 /* Use low pentatonic scale to make it not quite so annoying. */
@@ -92,11 +92,11 @@ static void menu_beep(uint16_t freq)
 }
 
 #ifdef QC
- void (*runningApp)() = QC_cb;
+#  define INITIAL_BADGE_APP QC_cb
 #else
- void (*runningApp)() = rvasec_splash_cb;
+#  define INITIAL_BADGE_APP rvasec_splash_cb;
 #endif
-
+void (*runningApp)() = INITIAL_BADGE_APP;
 
 #define MORE_INC 4
 
@@ -571,128 +571,4 @@ void genericMenu(struct menu_t *L_menu, MENU_STYLE style, uint32_t down_latches)
         }
     }
 }
-
-static const char splash_words1[] = "Loading";
-#define NUM_WORD_THINGS 18
-static const char *splash_word_things[] = {"Cognition Module",
-    "useless bits",
-    "backdoor.sh",
-    "exploit inside",
-    "sub-zero",
-    "lifting tables",
-    "personal data",
-    "important bits",
-    "bitcoin miner",
-    "GozNym",
-    "broken feature",
-    "NTFS", "Wall hacks",
-    "huawei 5G",
-    "Key logger",
-    "badgedows defender", "sshd", "cryptolocker", };
-    
-static const char splash_words_btn1[] = "Press the button";
-static const char splash_words_btn2[] = "to continue!";
-
-static void brand_preproduction_firmware(int blink)
-{
-	(void) blink;
-#define PREPRODUCTION_FIRMWARE 1
-#if PREPRODUCTION_FIRMWARE
-	if (!blink)
-		return;
-	FbColor(RED);
-	FbMove(13, 25);
-	FbWriteString("ALPHA FIRMWARE");
-	FbPushBuffer();
-#endif
-}
-
-#define SPLASH_SHIFT_DOWN 85
-static void rvasec_splash_cb(){
-    static unsigned short wait = 0;
-    static unsigned char loading_txt_idx = 0,
-    load_bar = 0;
-
-    if (wait == 0) {
-        load_bar = 10;
-        display_rect(0, 0, LCD_XSIZE, LCD_YSIZE);
-        display_color(0);
-        FbSwapBuffers();
-        led_pwm_enable(BADGE_LED_RGB_GREEN, 50 * 255/100);
-        //if(buzzer)
-        menu_beep(MORE_FREQ);
-    } else if(wait < 40){
-        FbImage4bit(&assetList[HACKRVA4], 0);
-        FbSwapBuffers();
-        //PowerSaveIdle();
-    } else if(wait < 80){
-        FbMove(0, 2);
-        FbImage2bit(&assetList[RVASEC2016], 0);
-        FbMove(10,SPLASH_SHIFT_DOWN);
-
-        FbColor(WHITE);
-        FbRectangle(100, 20);
-
-        FbMove(35, SPLASH_SHIFT_DOWN - 13);
-        FbColor(YELLOW);
-        FbWriteLine(splash_words1);
-
-        FbMove(11, SPLASH_SHIFT_DOWN+1);
-        FbColor(GREEN);
-        FbFilledRectangle((load_bar++ << 1) + 1,19);
-        led_pwm_enable(BADGE_LED_RGB_GREEN, 10 * 255 / 100);
-
-        FbColor(WHITE);
-        FbMove(4, 113);
-        FbWriteLine(splash_word_things[loading_txt_idx%NUM_WORD_THINGS]);
-        if(!(wait%2))
-            loading_txt_idx++;
-
-        FbSwapBuffers();
-
-    } else if(wait <160){
-        FbMove(0, 2);
-        FbImage2bit(&assetList[RVASEC2016], 0);
-        FbMove(10,SPLASH_SHIFT_DOWN);
-        FbColor(GREEN);
-        FbLine(0,60,132,60);
-        FbLine(0,62,132,62);
-        FbLine(0,65,132,65);
-        FbLine(0,69,132,69);
-        FbLine(0,77,132,77);
-
-        FbLine(105,60,145,77);
-        FbLine(95, 60,125,77);
-        FbLine(85, 60,105,77);
-        FbLine(75, 60,85,77);
-        FbLine(65, 60,65,77);
-        FbLine(55, 60,45,77);
-        FbLine(45, 60,25,77);
-        FbLine(35, 60,5,77);
-        FbLine(25, 60,0,65);
-
-        FbMove(1, 90);
-        FbWriteLine(splash_words_btn1);
-
-        FbMove(15, 110);
-        FbWriteLine(splash_words_btn2);
-
-        brand_preproduction_firmware(!((wait / 5) & 0x01) || wait > 158);
-        FbSwapBuffers();
-    }
-
-    wait++;
-    /* Don't let wait overflow, or else the splash animation will start over. */
-    if (wait == 0)
-        wait -= 1000;
-
-    // Sam: had some buzzer code here prior
-    int down_latches = button_down_latches();
-
-    if(BUTTON_PRESSED(BADGE_BUTTON_ENCODER_SW, down_latches) ||
-       BUTTON_PRESSED(BADGE_BUTTON_A, down_latches)){
-        returnToMenus();
-    }
-}
-
 
