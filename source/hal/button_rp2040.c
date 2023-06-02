269c33760d49e24deae5a34e51c7262f0ac8e873
Stephen M. Cameron <stephenmcameron@gmail.com>
2023-06-02 08:14:14 -0400

    button-debouncing: rename some variables and add some comments
    This is a purely cosmetic code change which should have no effect
    on execution.  The intent is to make clearer some things which have
    proven to be confusing on multiple occasionns.
    
    Signed-off-by: Stephen M. Cameron <stephenmcameron@gmail.com>

--- source/hal/button_rp2040.c
+++ source/hal/button_rp2040.c
@@ -13,7 +13,8 @@
 
 #define DEBOUNCE_DELAY_MS 3
 
-static const int8_t button_gpios[BADGE_BUTTON_MAX] = {
+/* Map enum BADGE_BUTTON_... to BADGE_GPIO_... (badge buttons to gpio pins) */
+static const int8_t button_to_gpio_pin[BADGE_BUTTON_MAX] = {
     /* A/B buttons */    
     BADGE_GPIO_BTN_A,
     BADGE_GPIO_BTN_B,
@@ -49,16 +50,16 @@ static int rotation_count[2];
 static user_gpio_callback user_cb;
 
 // forward declaration of GPIO callback since the alarm and GPIO handlers need to refer to each other
-static void gpio_callback(uint gpio, uint32_t events);
+static void gpio_callback(uint gpio_pin, uint32_t events);
 
-static void process_rotary_pin_state(uint gpio, int state) {
+static void process_rotary_pin_state(uint badge_button, int state) {
     int idx;
     int b;
 
-    if (gpio == BADGE_BUTTON_ENCODER_A) {
+    if (badge_button == BADGE_BUTTON_ENCODER_A) {
         idx = 0;
         b = BADGE_BUTTON_ENCODER_B;
-    } else if (gpio == BADGE_BUTTON_ENCODER_2_A) {
+    } else if (badge_button == BADGE_BUTTON_ENCODER_2_A) {
         idx = 1;
         b = BADGE_BUTTON_ENCODER_2_B;
     } else {
@@ -72,26 +73,26 @@ static void process_rotary_pin_state(uint gpio, int state) {
 
 int64_t alarm_callback(__attribute__((unused)) alarm_id_t id, void* user_data) {
 
-    uint gpio_enum = (uint)user_data;
-    uint gpio = button_gpios[gpio_enum];
+    uint badge_button = (uint) user_data;
+    uint gpio_pin = button_to_gpio_pin[badge_button];
 
-    int state = gpio_get(gpio) ? 1 : 0;
-    int prev_state = (gpio_states & (1u<<gpio_enum)) ? 1 : 0;
+    int state = gpio_get(gpio_pin) ? 1 : 0;
+    int prev_state = (gpio_states & (1u<<badge_button)) ? 1 : 0;
 
     if (state != prev_state) {
-        gpio_states &= ~(1<<gpio_enum);
-        gpio_states |= state<<gpio_enum;
+        gpio_states &= ~(1<<badge_button);
+        gpio_states |= state<<badge_button;
 
         if (state) {
-            up_latches |= 1<<gpio_enum;
+            up_latches |= 1<<badge_button;
         } else {
-            down_latches |= 1<<gpio_enum;
+            down_latches |= 1<<badge_button;
         }
 
         if (user_cb) {
-            user_cb(gpio_enum, state);
+            user_cb(badge_button, state);
         }
-        process_rotary_pin_state(gpio_enum, state);
+        process_rotary_pin_state(badge_button, state);
         last_change = rtc_get_ms_since_boot();
     }
 
@@ -99,7 +100,7 @@ int64_t alarm_callback(__attribute__((unused)) alarm_id_t id, void* user_data) {
         debounce_alarm_count--;
     }
 
-    gpio_set_irq_enabled_with_callback(gpio,
+    gpio_set_irq_enabled_with_callback(gpio_pin,
                                        GPIO_IRQ_EDGE_FALL | GPIO_IRQ_EDGE_RISE,
                                        true,
                                        gpio_callback);
@@ -107,16 +108,22 @@ int64_t alarm_callback(__attribute__((unused)) alarm_id_t id, void* user_data) {
 }
 
 void gpio_callback(uint gpio, __attribute__((unused)) uint32_t events) {
-    for (int i=0; i<BADGE_BUTTON_MAX; i++) {
-        uint gpio_pin = (uint)(button_gpios[i]);
+    for (int button = 0; button < BADGE_BUTTON_MAX; button++) {
+        uint gpio_pin = (uint)(button_to_gpio_pin[button]);
         if (gpio_pin == gpio) {
             gpio_set_irq_enabled(gpio, GPIO_IRQ_EDGE_FALL | GPIO_IRQ_EDGE_RISE, false);
             // After time delay, check for bounce being done.
             debounce_alarm_count++;
+
+            /* It is explicitly OK to call this here, even from interrupt context
+             * and even from an alarm callback.
+             * See: pico-sdk-src/src/common/pico_time/include/pico/time.h line 477
+             * and surrounding comments.
+             */
             alarm_pool_add_alarm_in_ms(alarm_pool_get_default(),
                                        DEBOUNCE_DELAY_MS,
                                        alarm_callback,
-                                       (void*)i,
+                                       (void*) button,
                                        true);
         }
     }
@@ -128,7 +135,7 @@ void button_init_gpio(void) {
     critical_section_init(&critical_section);
 
     for (int i=0; i<BADGE_BUTTON_MAX; i++) {
-        uint gpio = (uint) button_gpios[i];
+        uint gpio = (uint) button_to_gpio_pin[i];
         gpio_init(gpio);
         gpio_set_input_enabled(gpio, 1);
         gpio_set_input_hysteresis_enabled(gpio, 1);
@@ -136,8 +143,8 @@ void button_init_gpio(void) {
     }
 
     for (int i=0; i<BADGE_BUTTON_MAX; i++) {
-        uint gpio = (uint) button_gpios[i];
-        uint state = gpio_get(button_gpios[i]) ? 1 : 0;
+        uint gpio = (uint) button_to_gpio_pin[i];
+        uint state = gpio_get(button_to_gpio_pin[i]) ? 1 : 0;
         gpio_states |= (state<<i);
         gpio_set_irq_enabled_with_callback(gpio, GPIO_IRQ_EDGE_FALL | GPIO_IRQ_EDGE_RISE, true,
                                            gpio_callback);
