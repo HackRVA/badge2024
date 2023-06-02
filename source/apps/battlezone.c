aeeb9a1ed7d2d5f7fe86ba4447359ee0690cf8b1
Stephen M. Cameron <stephenmcameron@gmail.com>
2023-06-02 09:57:39 -0400

    battlezone: allow steering and shooting with rotary knobs
    Signed-off-by: Stephen M. Cameron <stephenmcameron@gmail.com>

--- source/apps/battlezone.c
+++ source/apps/battlezone.c
@@ -762,19 +762,22 @@ static void fire_gun(void)
 static void check_buttons(void)
 {
 	int down_latches = button_down_latches();
+	int r0 = button_get_rotation(0);
+	int r1 = button_get_rotation(1);
 	if (BUTTON_PRESSED(BADGE_BUTTON_ENCODER_SW, down_latches)) {
 		battlezone_state = BATTLEZONE_EXIT;
 		return;
 	}
-	if (BUTTON_PRESSED(BADGE_BUTTON_A, down_latches)) {
+	if (BUTTON_PRESSED(BADGE_BUTTON_A, down_latches) ||
+		BUTTON_PRESSED(BADGE_BUTTON_ENCODER_2_SW, down_latches)) {
 		fire_gun();
 	}
-	if (button_poll(BADGE_BUTTON_LEFT)) {
+	if (button_poll(BADGE_BUTTON_LEFT) || r1 < 0 || r0 < 0) {
 		camera.orientation--;
 		if (camera.orientation < 0)
 			camera.orientation = 127;
 	}
-	if (button_poll(BADGE_BUTTON_RIGHT)) {
+	if (button_poll(BADGE_BUTTON_RIGHT) || r1 > 0 || r0 > 0) {
 		camera.orientation++;
 		if (camera.orientation > 127)
 			camera.orientation = 0;
