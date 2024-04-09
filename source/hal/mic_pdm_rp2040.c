733e3205eccfcaed1d6dfc0bb684bc1f74eb615c
bleem313 <bleem313@gmail.com>
2024-04-09 17:13:23 -0400

    initial mic setup

---
+++ source/hal/mic_pdm_rp2040.c
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2021 Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * This examples captures data from a PDM microphone using a sample
+ * rate of 8 kHz and prints the sample values over the USB serial
+ * connection.
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+#include "pico/stdlib.h"
+#include "pico/pdm_microphone.h"
+
+// configuration
+const struct pdm_microphone_config config = {
+    // GPIO pin for the PDM DAT signal
+    .gpio_data = 3,
+
+    // GPIO pin for the PDM CLK signal
+    .gpio_clk = 4,
+
+    // PIO instance to use
+    .pio = pio1,
+
+    // PIO State Machine instance to use
+    .pio_sm = 0,
+
+    // sample rate in Hz
+    .sample_rate = 8000,
+
+    // number of samples to buffer
+    .sample_buffer_size = 256,
+};
+
+// variables
+int16_t sample_buffer[256];
+volatile int samples_read = 0;
+
+void on_pdm_samples_ready()
+{
+    // callback from library when all the samples in the library
+    // internal sample buffer are ready for reading
+    samples_read = pdm_microphone_read(sample_buffer, 256);
+}
+
+void mic_init(void){
+    pdm_microphone_init(&config);
+    pdm_microphone_set_samples_ready_handler(on_pdm_samples_ready);
+    pdm_microphone_start();
+};
