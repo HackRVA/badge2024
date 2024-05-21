8988a26f0e2f2a32aacad00995246c99bbb24922
Peter Maxwell Warasila <madmaxwell@soundcomesout.com>
2024-05-20 21:01:01 -0400

    mic: add specific return codes to API functions
    Signed-off-by: Peter Maxwell Warasila <madmaxwell@soundcomesout.com>

--- source/hal/mic_pdm_sim.c
+++ source/hal/mic_pdm_sim.c
@@ -25,14 +25,14 @@ void mic_stop(void){
 int mic_add_cb(mic_callback_t cb)
 {
     if (NULL == cb) {
-        return -1;
+        return MIC_RC_EPARAM_NULL;
     }
 
     size_t available = SIZE_MAX;
     for (size_t i = 0; i < (sizeof(mic_cb_table) / sizeof(mic_cb_table[0])); i++) {
         if (mic_cb_table[i] == cb) {
             /* This callback is already used as an entry. */
-            return -2;
+            return MIC_RC_EALREADY_EXISTS;
         } else if ((NULL == mic_cb_table[i]) && (available == SIZE_MAX)) {
             available = i;
         }
@@ -40,28 +40,28 @@ int mic_add_cb(mic_callback_t cb)
 
     if (available == SIZE_MAX) {
         /* There is no space in the table. */
-        return -3;
+        return MIC_RC_ENO_SPACE;
     }
 
     /* Assign the available entry. */
     mic_cb_table[available] = cb;
-    return 0;
+    return MIC_RC_OK;
 }
 
 int mic_remove_cb(mic_callback_t cb)
 {
     if (NULL == cb) {
-        return -1;
+        return MIC_RC_EPARAM_NULL;
     }
 
     for (size_t i = 0; i < (sizeof(mic_cb_table) / sizeof(mic_cb_table[0])); i++) {
         if (mic_cb_table[i] == cb) {
             mic_cb_table[i] = NULL;
-            return 0;
+            return MIC_RC_OK;
         }
     }
 
     /* Did not find callback in table. */
-    return -2;
+    return MIC_RC_ENOT_THERE;
 }
 
